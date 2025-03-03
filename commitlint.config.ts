a391439b9cd79d968f4ef1f687bac4676163e43b
yinchengfeng-bytedance <yinchengfeng@bytedance.com>
2025-03-03 17:59:10 +0800

    feat(arkitect): support commitlint (#55)
    * feat(arkitect): add commitlint support
    
    * feat(arkitect): license check support js

---
+++ commitlint.config.ts
@@ -0,0 +1,49 @@
+import type { UserConfig } from '@commitlint/types';
+import { RuleConfigSeverity } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: 'conventional-changelog-atom',
+    formatter: '@commitlint/format',
+    rules: {
+        'type-enum': [
+            RuleConfigSeverity.Error,
+            'always',
+            [
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'release',
+                'license',
+            ],
+        ],
+        'scope-case': [RuleConfigSeverity.Error, 'always', 'lower-case'],
+        'subject-max-length': [RuleConfigSeverity.Error, 'always', 100],
+        'validate-scope': [RuleConfigSeverity.Error, 'always'],
+    },
+    plugins: [
+        {
+            rules: {
+                'validate-scope': c => {
+                    // Allow empty scopes for CI and Release types
+                    if (!c.scope) return [['release', 'license'].includes(c.type), 'scope must be provided']
+
+                    const allowedScopes = ['arkitect', 'demohouse/*'];
+                
+                    // 正则校验：arkitect 或 demohouse/ 开头
+                    const isValid = /^(arkitect|demohouse\/.+)$/.test(c.scope);
+                    return [isValid, `Scope must be one of ${allowedScopes.join(', ')}`];
+                },
+            }
+        }
+    ],
+    helpUrl: 'https://github.com/volcengine/ai-app-lab/docs/commitlint.md',
+};
+
+export default Configuration;
