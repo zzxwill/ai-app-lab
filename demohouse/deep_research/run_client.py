920296a68e59249b21ca1a158c4c9b9d538db891
wgzesg-bd <zesong.wang1@bytedance.com>
2025-03-05 23:21:23 +0800

    fix(demohouse/deepresearch): reasoning content empty

--- demohouse/deep_research/run_client.py
+++ demohouse/deep_research/run_client.py
@@ -8,38 +8,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from openai import OpenAI
 
-client = OpenAI(base_url="http://localhost:8888/api/v3/bots", api_key="{API_KEY}")
+# 注意baseUrl
+client = OpenAI(base_url="http://localhost:8888/api/v3/bots", api_key="{YOUR_API_KEY}")
 
 
 def main():
-    # stream run
     stream_resp = client.chat.completions.create(
         model="test",
         messages=[
             {
                 "role": "user",
-                "content": "帮我查一下2024年11月上市的智能手机的价格，并给出一篇有关其中最便宜的一款的网络评测",
+                "content": "找到2023年中国GDP超过万亿的城市，详细分析其中排名后十位的城市的增长率和GDP构成，并结合各城市规划预测5年后这些城市的GDP排名可能会如何变化",
             }
         ],
         stream=True,
     )
-
     thinking = False
-
     for chunk in stream_resp:
-        if chunk.choices[0].delta.reasoning_content:
+        if chunk.choices[0].delta.model_extra.get("reasoning_content"):
             if not thinking:
                 print("\n----思考过程----\n")
                 thinking = True
-            print(chunk.choices[0].delta.reasoning_content, end="")
+            content = chunk.choices[0].delta.model_extra.get("reasoning_content", "")
+            print(content, end="", flush=True)
         elif chunk.choices[0].delta.content:
             if thinking:
                 print("\n----输出回答----\n")
                 thinking = False
-            print(chunk.choices[0].delta.content, end="")
+            print(chunk.choices[0].delta.content, end="", flush=True)
 
 
 if __name__ == "__main__":
