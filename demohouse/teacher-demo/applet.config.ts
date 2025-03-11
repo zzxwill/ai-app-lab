8f53e5595799fa446e7b285267bdb8aa00175a77
yunhe <yunhe@bytedance.com>
2025-03-11 14:21:14 +0800

    feat(demohouse/teacher_app): init teacher demo

--- demohouse/teacher-demo/applet.config.ts
+++ demohouse/teacher-demo/applet.config.ts
@@ -9,21 +9,6 @@ export default defineConfig({
       { type: 'FullPage', id: 'recognition' }
   },
   deploy: {
-    gecko: {
-      env: 'prod',
-      deployAccessKey: '902679bb2254666f3e4642f82008a743',
-      deploymentId: 25828116277250,
-      channel: 'com_volcengine_snap_solver',
-      channelId: '26322102019842',
-      prefix: 'gecko/applet/volcengine',
-      leafId: '26322102018306',
-      // env: 'test',
-      // deployAccessKey: '3d40cd5a01416112cebfd266e9acbb94',
-      // deploymentId: 25828116277506,
-      // channel: 'com_volcengine_snap_solver',
-      // channelId: '26047372999426',
-      // prefix: 'gecko/applet/volcengine',
-      // leafId: '26047372997890',
-    }
+    
   }
 });
